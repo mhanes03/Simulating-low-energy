42dac8d1822b61928671f7ed9fe5a4285a4d75b8
mhanes03 <mh2599@york.ac.uk>
2025-09-22 16:03:10 +0100

    matching stopping power and electric field

---
+++ matching_stopping_powers_and_electric/sample_stp_two.sh
@@ -0,0 +1,28 @@
+# !/bin/bash
+
+electric_fields="1.204 24.076"
+
+for electric in $electric_fields
+do
+
+    # makes directory for stopping power
+    mkdir $electric
+
+    # moves into the directory for stopping power being sampled
+    cd $electric
+
+    # coping the input file into the directory corresponds to stopping power being run
+    cp -r ../low_energy.g4bl ../BEAM_100K.txt .
+
+    # prints the current working directory
+    pwd 
+
+    # prints which radiusthe simulation is being run with 
+    echo "Running simulation for electric field = "$electric 
+
+    # runs simulation in the correct radius directory
+    apptainer run --app g4blmpi ~/g4blmpi_muoncooling_20250821.sif 16 low_energy.g4bl E_field_magnitude=$electric > g4_out
+
+    # move to previous directory
+    cd ..
+done
