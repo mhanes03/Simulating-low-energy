23c6696d914e3769504ea869df67d66d0a35c985
mhanes03 <mh2599@york.ac.uk>
2026-07-07 11:36:41 +0100

    Create make_TRIM_file.py
    trim.dat script maker

---
+++ Python_moving_SRIM_and_G4/make_TRIM_file.py
@@ -0,0 +1,31 @@
+import numpy as np 
+
+# for the gaussian distribution, energy in eV
+mean = 4E6
+sigma = 0.02*4E6
+samples = 10000
+
+# For the TRIM.dat file column to specify hydrogen
+atom_number = 1
+
+# depth values
+X = 0 
+Y = 0 
+Z = 0 
+
+# cosx,y and z for atom direction 
+x_direction = 1
+y_direction = 0 
+z_direction = 0 
+
+# creates gaussian 
+gauss = np.random.normal(mean, sigma, samples)
+
+open('TRIM.dat', 'x')
+
+for event_name in range(1, samples, 1):
+
+	with open('TRIM.dat', 'a') as f:
+		f.write('H    ' + ' ' + str(atom_number) + ' ' + str(int(gauss[event_name])) + ' ' + str(X) + ' ' + str(Y) + ' ' + str(Z) + ' ' + str(x_direction) +  ' ' + str(y_direction) + ' ' + str(z_direction) + '\r\n')
+
+f.close()
