fa93588f4d7f468de7694522f627dc2d64599bee
mhanes03 <mh2599@york.ac.uk>
2026-07-07 15:03:15 +0100

    Create sort_time.sh

---
+++ Awk_scripts/sort_time.sh
@@ -0,0 +1,15 @@
+mkdir tntuples
+
+cp time_output.txt tntuples
+
+cd tntuples 
+
+awk '$1**2+$2**2 <= 500**2 && $3 >= 0 && $3 <= 1000' time_output.txt > in_cell_times.txt
+
+sort -k 7n in_cell_times.txt > in_cell_times_sorted.txt
+
+for i in $(seq 0 10 2000)
+do 
+	awk '$7 == '$i in_cell_times_sorted.txt > T$i.txt 
+
+done 
