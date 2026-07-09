f3b9ba477357f8c30c0c7745dc673a3efc3aebf6
mhanes03 <mh2599@york.ac.uk>
2026-07-09 16:29:25 +0100

    adding comments

--- Python_moving_SRIM_and_G4/convert_beam_file.sh
+++ Python_moving_SRIM_and_G4/convert_beam_file.sh
@@ -1,3 +1,7 @@
+# Purpose : to remove the T from the TRANSMIT file and remove the white space in the first column, and run the python script to produce the beam file
+#
+# Input : TRANSMIT file from SRIM 
+# Output : the new TRANSMIT file that removes the first column, and then beam file from the python file 
 awk '{OFS=" " ; $1=""; gsub(/[[:space:]]+/, " "); print $0}' TRANSMIT.txt > new_TRANSMIT.txt
 
 python3 ~/python/convert_to_BLTtrack.py
