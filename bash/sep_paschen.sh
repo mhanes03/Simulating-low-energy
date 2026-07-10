6b0f1155655810f9e2d31a33dd498633e85dd52b
mhanes03 <mh2599@york.ac.uk>
2026-07-10 10:52:32 +0100

    Update sep_paschen.sh

--- bash/sep_paschen.sh
+++ bash/sep_paschen.sh
@@ -1,3 +1,9 @@
+# Purpose : separates the Paschen factor values into different files 
+#
+# Input : optimisation trails   
+# Output : Paschen files 
+# 
+# Limitations : 
 python3 ~/python/round.py
 
 for i in $(seq 1 0.1 5)
