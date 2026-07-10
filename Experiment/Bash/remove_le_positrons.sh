9c7297723c8a237b2ce1804ee102a06f297da8d9
mhanes03 <mh2599@york.ac.uk>
2026-07-10 10:17:07 +0100

    Update remove_le_positrons.sh

--- Experiment/Bash/remove_le_positrons.sh
+++ Experiment/Bash/remove_le_positrons.sh
@@ -1,3 +1,9 @@
+# Purpose : removes the low energy positrons from the detector file, by looking at their initial energy 
+#
+# Input : detector files  
+# Output : detector files without the low energy positrons 
+# 
+# Limitations : 
 awk '$28 > 26' Det1.txt > Det1_remove_le.txt
 
 awk '$28 > 26' Det2.txt > Det2_remove_le.txt
