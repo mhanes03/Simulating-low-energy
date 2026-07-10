58dc4a81092932f9e12222304e96bb4253a1881e
mhanes03 <mh2599@york.ac.uk>
2026-07-10 12:45:33 +0100

    Create vary_sp.sh

---
+++ bash/vary_sp.sh
@@ -0,0 +1,30 @@
+# !/bin/bash
+# Purpose : to loop over the stopping power values, that will match the electric field in the input file 
+#
+# Input : input file 
+# Output : the simulation 
+
+for sp in $(seq 20 5 45)
+do
+
+    # makes directory for stopping power
+    mkdir $sp
+
+    # moves into the directory for stopping power being sampled
+    cd $sp
+
+    # coping the input file into the directory corresponds to stopping power being run
+    cp ../muon_drift_He_test.g4bl .
+
+    # prints the current working directory
+    pwd 
+
+    # prints which radiusthe simulation is being run with 
+    echo "Running simulation for stopping power = "$sp 
+
+    # runs simulation in the correct radius directory
+    apptainer run --app g4blmpi ~/g4blmpi_muoncooling_20250821.sif 16 muon_drift_He_test.g4bl stp=$sp > g4_out
+
+    # move to previous directory
+    cd ..
+done
