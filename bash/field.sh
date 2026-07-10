a9ee511ac4cadbc1b72bc318fea9a2d39928177c
mhanes03 <mh2599@york.ac.uk>
2026-07-10 10:36:24 +0100

    Update field.sh

--- bash/field.sh
+++ bash/field.sh
@@ -1,4 +1,9 @@
-
+# Purpose : produces the field files for each z value
+#
+# Input : fieldntuple from simulation 
+# Output : field files  
+# 
+# Limitations : 
 for i in $(seq 0 10 1000)
 do 
 	awk '$3 == '$i field.txt > F$i.txt 
