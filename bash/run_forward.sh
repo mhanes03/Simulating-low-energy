64c53c1e37198e43f9c56d8fda3f74e1fc2dec67
mhanes03 <mh2599@york.ac.uk>
2026-07-10 10:55:18 +0100

    Update run_forward.sh

--- bash/run_forward.sh
+++ bash/run_forward.sh
@@ -1,3 +1,9 @@
+# Purpose : runs the python file and runds g4bl simulation 
+#
+# Input : python script and input file for g4bl  
+# Output : beam file from the python script 
+# 
+# Limitations : 
 python3 ~/python/make_beam__file_distr.py
 
 g4bl input.g4bl > g4_out
